5980de07163a144e4a286af53db9ee76326e38ee
Marçal Albert Castellví <mac.albert@gmail.com>
2022-02-24 11:37:09 +0100

    test: refactor

--- S3Workshop.Tests/AwsS3StorageTests.cs
+++ S3Workshop.Tests/AwsS3StorageTests.cs
@@ -10,37 +10,38 @@
 
     public class AwsS3StorageTests
     {
-        private readonly string _bucketIntegration = "repintel-integration-tests";
+        private const string _bucketIntegration = "workshop-demo-2022";
         private readonly AwsS3Storage _storage;
 
         public AwsS3StorageTests()
         {
-            var configuration = new ConfigurationBuilder()
+            var services = new ServiceCollection();
+
+            services.AddDefaultAWSOptions(new ConfigurationBuilder()
                     .SetBasePath(Directory.GetCurrentDirectory())
                     .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                    .Build();
-            var services = new ServiceCollection();
-            services.AddDefaultAWSOptions(configuration.GetAWSOptions());
+                    .Build().GetAWSOptions());
             services.AddAWSService<IAmazonS3>();
             services.AddScoped<AwsS3Storage>();
-            var provider = services.BuildServiceProvider();
 
-            _storage = provider.GetService<AwsS3Storage>()!;
+            _storage = services.BuildServiceProvider()
+                .GetService<AwsS3Storage>()!;
         }
 
         [Fact]
         public async Task Should_Save_When_JsonText()
         {
+            // Arrange
             var fileName = Guid.NewGuid().ToString();
-            var expectedContent = @"{""fruit"": ""Apple"",""size"": ""Large"",""color"": ""Red🤙 💪 🦵 🦶 🖕 ✍️ 🙏 💍 💄 💋 👄 👅 👂 👃 👣 👁 👀 🧠 🦴 🦷 🗣 👤 👥""}";
+            var expected = @"{""field"": ""value 💪 🦵 🦶 🖕 ✍️ 🙏 💍 👅 👂 👃 👣 👁 👀 🧠 🦴 🦷 🗣 👤 👥""}";
 
-            await _storage.SaveAsync(_bucketIntegration, fileName, S3CannedACL.PublicRead, expectedContent);
-
-            var actualContent = await _storage.GetAsync(_bucketIntegration, fileName);
-
+            // Act
+            await _storage.SaveAsync(_bucketIntegration, fileName, S3CannedACL.PublicRead, expected);
+            var actual = await _storage.GetAsync(_bucketIntegration, fileName);
             await _storage.DeleteAsync(_bucketIntegration, fileName);
 
-            Assert.Equal(expectedContent, actualContent);
+            // Assert
+            Assert.Equal(expected, actual);
         }
     }
 }
